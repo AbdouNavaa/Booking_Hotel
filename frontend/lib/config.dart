183f390eb5d6b862d05ec00549ca3ee15dad74bb
Abdou <babana9977@gmail.com>
2023-02-16 23:58:18 +0000

    Delete all

--- frontend/lib/config.dart
+++
@@ -1,7 +0,0 @@
-class Config{
-  static const String appName = "Hotel Booking";
-  static const String apiURL = "192.168.43.73:8000";
-  static const String loginAPI = "/accounts/login/";
-  static const String registerAPI = "/registration/";
-  // static const String userProfileAPI = "/users/user-profile";
-}
